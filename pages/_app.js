a114880acf4bb5dad16e028f8a5d4bc26fff6aff
Daniel Havelda <8553631dev@gmail.com>
2021-12-15 16:01:56 +0100

    Deleted unused files, codes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+import Head from 'next/head';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <style>
+          @import url('https://fonts.googleapis.com/css2?family=Karla:wght@400;500;700&display=swap');
+        </style>
+      </Head>
+      <Component { ...pageProps } />
+    </>
+  );
 }
 
 export default MyApp
