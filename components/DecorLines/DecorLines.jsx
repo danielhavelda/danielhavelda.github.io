06738be1c2b4ed5464746204c0a5d7fb3078efa9
Daniel Havelda <8553631dev@gmail.com>
2022-01-07 22:09:44 +0100

    Fixed DecorLines position and utility classes

--- components/DecorLines/DecorLines.jsx
+++ components/DecorLines/DecorLines.jsx
@@ -2,7 +2,7 @@ import classes from './DecorLines.module.scss';
 
 export default function DecorLines() {
   return (
-    <div className={classes["decor-lines"]}>
+    <div className={classes['decor-lines']}>
       <div className={classes["line"]}></div>
       <div className={classes["line"]}></div>
       <div className={classes["line"]}></div>
