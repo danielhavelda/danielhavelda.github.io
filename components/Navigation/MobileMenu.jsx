ed652d6a2941ce90df810b631e735051f601fb6e
Daniel Havelda <8553631dev@gmail.com>
2022-01-07 22:18:22 +0100

    Created MobileMenu prototype component

---
+++ components/Navigation/MobileMenu.jsx
@@ -0,0 +1,26 @@
+import classes from './MobileMenu.module.scss';
+import { useState } from 'react';
+import DecorLines from '../DecorLines/DecorLines';
+
+const MobileMenu = ({ navigationSection }) => {
+  const [navDisplay, setNavDisplay] = useState(false);
+
+  return (
+    <div className={classes["mobile-menu"]}>
+      <div className={classes['test-btn']} onClick={() => setNavDisplay((prev) => !prev)}>
+        <span className={classes['line']}></span>
+        <span className={classes['line']}></span>
+        <span className={classes['line']}></span>
+      </div>
+      <div className={ 
+        navDisplay ? classes['navigation-section'] : [classes['navigation-section'], [classes['closed']]].join(' ')
+      }
+      >
+        <DecorLines />
+        {navigationSection}
+      </div>
+    </div>
+  );
+};
+
+export default MobileMenu;
--- components/Navigation/Navigation.jsx
+++ components/Navigation/Navigation.jsx
@@ -1,21 +1,31 @@
 import classes from './Navigation.module.scss';
 import Link from 'next/link';
+import MobileMenu from './MobileMenu';
+import useWindowSize from '../../hooks/useWindowSize';
 
 export default function Navigation(props) {
+  const [screenWidth, screenHeight] = useWindowSize(500);
+
+  const navigationContent = (
+    <ul>
+      <li>
+        <Link href="#">
+          <a className='font-medium text-gray-300'>Munkáim</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/about-me">
+          <a className='font-medium text-gray-300'>Bemutatkozás</a>
+        </Link>
+      </li>
+    </ul>
+  );
+
   return (
-    <nav className={classes['navigation']}>
-      <ul>
-        <li>
-          <Link href="#">
-            <a className='font-medium text-gray-300'>Munkáim</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/about-me">
-            <a className='font-medium text-gray-300'>Bemutatkozás</a>
-          </Link>
-        </li>
-      </ul>
-    </nav>
+      <nav className={classes['navigation']}>
+        {
+          screenWidth > 768 ? navigationContent : <MobileMenu navigationSection={navigationContent}/>
+        }
+      </nav>
   );
 }
