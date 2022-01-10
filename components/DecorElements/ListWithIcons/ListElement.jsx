74deabc151e53b7e17c62499798591bf242b6615
Daniel Havelda <8553631dev@gmail.com>
2022-01-10 18:18:27 +0100

    Fixed DecorElements and DecorLines components

--- components/DecorElements/ListWithIcons/ListElement.jsx
+++ components/DecorElements/ListWithIcons/ListElement.jsx
@@ -1,8 +1,10 @@
+import classes from './ListElement.module.scss';
+
 const ListElement = (props) => (
   <li>
     {props.icon}
     {
-      props.href === undefined ? <p className="text-white">{props.text}</p> : <a href={props.href} className="text-white">{props.href}</a> 
+      props.href === undefined ? <p className={classes["list-element"]}>{props.text}</p> : <a href={props.href} className={["list-element"]}>{props.href}</a> 
     }
   </li>
 );
